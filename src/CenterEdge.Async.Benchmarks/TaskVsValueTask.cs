ad9e0d7e7559feb4727336170b01dc60468c9e8e
Brant Burnett <bburnett@centeredgesoftware.com>
2021-07-29 16:34:02 -0400

    Add a Task vs ValueTask benchmark

---
+++ src/CenterEdge.Async.Benchmarks/TaskVsValueTask.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+
+namespace CenterEdge.Async.Benchmarks
+{
+    public class TaskVsValueTask
+    {
+        [Benchmark(Baseline = true)]
+        public int Task()
+        {
+            return AsyncHelper.RunSync(AsyncTaskMethod);
+        }
+
+        [Benchmark]
+        public int ValueTask()
+        {
+            return AsyncHelper.RunSync(AsyncValueTaskMethod);
+        }
+
+        private async Task<int> AsyncTaskMethod()
+        {
+            var i = 1;
+            await System.Threading.Tasks.Task.Yield();
+
+            i++;
+
+            return i;
+        }
+
+        private async ValueTask<int> AsyncValueTaskMethod()
+        {
+            var i = 1;
+            await System.Threading.Tasks.Task.Yield();
+
+            i++;
+
+            return i;
+        }
+    }
+}

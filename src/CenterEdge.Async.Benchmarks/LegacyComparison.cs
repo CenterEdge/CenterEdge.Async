f0d6c7db7efd0229eb29274c7e47979e9a99a65c
Brant Burnett <bburnett@centeredgesoftware.com>
2021-07-29 16:24:04 -0400

    Add benchmarks
    Results:
    
    BenchmarkDotNet=v0.13.0, OS=Windows 10.0.19043.1110 (21H1/May2021Update)
    Intel Core i7-9750H CPU 2.60GHz, 1 CPU, 12 logical and 6 physical cores
    .NET SDK=6.0.100-preview.5.21302.13
      [Host]     : .NET 5.0.8 (5.0.821.31504), X64 RyuJIT
      Job-DTXPAT : .NET 5.0.8 (5.0.821.31504), X64 RyuJIT
      Job-LAWAMJ : .NET Core 3.1.17 (CoreCLR 4.700.21.31506, CoreFX 4.700.21.31502), X64 RyuJIT
      Job-UYUMYU : .NET Framework 4.8 (4.8.4390.0), X64 RyuJIT
    
    | Method |        Job |            Runtime |    Toolchain |       Mean |    Error |   StdDev | Ratio |
    |------- |----------- |------------------- |------------- |-----------:|---------:|---------:|------:|
    |    New | Job-DTXPAT |           .NET 5.0 | netcoreapp50 |   576.0 ns |  2.05 ns |  1.60 ns |  0.15 |
    | Legacy | Job-DTXPAT |           .NET 5.0 | netcoreapp50 | 3,307.4 ns | 14.26 ns | 11.91 ns |  0.83 |
    |    New | Job-LAWAMJ |      .NET Core 3.1 | netcoreapp31 |   605.8 ns |  2.74 ns |  2.29 ns |  0.15 |
    | Legacy | Job-LAWAMJ |      .NET Core 3.1 | netcoreapp31 | 3,340.2 ns | 39.61 ns | 33.07 ns |  0.84 |
    |    New | Job-UYUMYU | .NET Framework 4.8 |        net48 | 1,182.0 ns | 23.64 ns | 29.03 ns |  0.30 |
    | Legacy | Job-UYUMYU | .NET Framework 4.8 |        net48 | 3,966.4 ns | 37.35 ns | 31.19 ns |  1.00 |

---
+++ src/CenterEdge.Async.Benchmarks/LegacyComparison.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+
+namespace CenterEdge.Async.Benchmarks
+{
+    public class LegacyComparison
+    {
+        [Benchmark]
+        public int New()
+        {
+            return AsyncHelper.RunSync(AsyncMethod);
+        }
+
+        [Benchmark(Baseline = true)]
+        public int Legacy()
+        {
+            return LegacyAsyncHelper.RunSync(AsyncMethod);
+        }
+
+        private async Task<int> AsyncMethod()
+        {
+            var i = 1;
+            await Task.Yield();
+
+            i++;
+
+            return i;
+        }
+    }
+}
---
+++ src/CenterEdge.Async.Benchmarks/Program.cs
@@ -0,0 +1,4 @@
+﻿using BenchmarkDotNet.Running;
+using CenterEdge.Async.Benchmarks;
+
+BenchmarkSwitcher.FromAssembly(typeof(LegacyComparison).Assembly).Run(args);
